4d3d8426dd1162c41f9736180658ed2104c03f22
Rahul Vasist <rahulrav@Aiswaryas-MacBook-Air.local>
2021-12-12 18:00:34 -0800

    using aoc::main

--- src/day12.rs
+++ src/day12.rs
@@ -1,140 +1,140 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-};
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let s = fs::read_to_string(r#"/Users/rahulrav/rust/aoc2021/data/day12.txt"#)?;
-    println!("part1 {}", part1(&CaveSystem::new(&s)));
-    println!("part2 {}", part2(&CaveSystem::new(&s)));
-    Ok(())
+aoc_main::main! {
+    year 2021;
+    day12 : generator => part_1, part_2;
 }
 
-#[derive(PartialEq, Eq, Hash, Debug)]
-struct Cave(String);
+mod day12 {
+    use std::collections::{HashMap, HashSet};
 
-impl Cave {
-    fn is_small_cave(&self) -> bool {
-        self.0.chars().next().unwrap().is_lowercase()
-    }
-}
+    #[derive(PartialEq, Eq, Hash, Debug)]
+    pub struct Cave(String);
 
-#[derive(Debug)]
-struct CaveSystem {
-    caves: HashSet<Cave>,
-    connections: HashMap<Cave, Vec<Cave>>,
-}
-
-impl CaveSystem {
-    fn new(s: &str) -> Self {
-        let mut caves: HashSet<Cave> = HashSet::new();
-        let mut connections: HashMap<Cave, Vec<Cave>> = HashMap::new();
-        for l in s.lines() {
-            let mut tokens = l.split('-');
-            let c0 = String::from(tokens.next().unwrap());
-            let c1 = String::from(tokens.next().unwrap());
-            caves.insert(Cave(c0.clone()));
-            caves.insert(Cave(c1.clone()));
-            let entry = connections.entry(Cave(c0.clone())).or_insert(Vec::new());
-            entry.push(Cave(c1.clone()));
-            let entry = connections.entry(Cave(c1.clone())).or_insert(Vec::new());
-            entry.push(Cave(c0.clone()));
+    impl Cave {
+        fn is_small_cave(&self) -> bool {
+            self.0.chars().next().unwrap().is_lowercase()
         }
-        Self { caves, connections }
     }
-}
 
-fn paths2<'a>(
-    cave: &'a Cave,
-    cs: &'a CaveSystem,
-    visited: &mut HashMap<&'a Cave, usize>,
-    path: &mut Vec<&'a Cave>,
-    s_cave_used_twice: bool,
-) -> usize {
-    // println!("{} {:?} {}", cave.0, visited, s_cave_used_twice);
-    if cave.0 == "end" {
-        // for c in path {
-        //     print!("{}-", c.0);
-        // }
-        // println!("end");
-        return 1;
+    #[derive(Debug)]
+    pub struct CaveSystem {
+        caves: HashSet<Cave>,
+        connections: HashMap<Cave, Vec<Cave>>,
     }
-    path.push(cave);
-    if cave.is_small_cave() {
-        let entry = visited.entry(cave).or_insert(0);
-        *entry += 1;
-    }
-    let mut rv = 0;
-    for con in cs.connections.get(cave).unwrap() {
-        if con.0 == "start" {
-            continue;
-        }
-        if visited.contains_key(con) {
-            if s_cave_used_twice {
-                continue;
-            } else {
-                // Let's use this small cave twice
-                rv += paths2(con, cs, visited, path, true);
+
+    impl CaveSystem {
+        fn new(s: &str) -> Self {
+            let mut caves: HashSet<Cave> = HashSet::new();
+            let mut connections: HashMap<Cave, Vec<Cave>> = HashMap::new();
+            for l in s.lines() {
+                let mut tokens = l.split('-');
+                let c0 = String::from(tokens.next().unwrap());
+                let c1 = String::from(tokens.next().unwrap());
+                caves.insert(Cave(c0.clone()));
+                caves.insert(Cave(c1.clone()));
+                let entry = connections.entry(Cave(c0.clone())).or_insert(Vec::new());
+                entry.push(Cave(c1.clone()));
+                let entry = connections.entry(Cave(c1.clone())).or_insert(Vec::new());
+                entry.push(Cave(c0.clone()));
             }
-        } else {
-            rv += paths2(con, cs, visited, path, s_cave_used_twice);
+            Self { caves, connections }
         }
     }
-    path.pop();
-    if cave.is_small_cave() {
-        let ref_count = visited.get_mut(cave).unwrap();
-        *ref_count -= 1;
-        if *ref_count == 0 {
-            visited.remove(cave);
+
+    fn paths2<'a>(
+        cave: &'a Cave,
+        cs: &'a CaveSystem,
+        visited: &mut HashMap<&'a Cave, usize>,
+        path: &mut Vec<&'a Cave>,
+        s_cave_used_twice: bool,
+    ) -> usize {
+        // println!("{} {:?} {}", cave.0, visited, s_cave_used_twice);
+        if cave.0 == "end" {
+            // for c in path {
+            //     print!("{}-", c.0);
+            // }
+            // println!("end");
+            return 1;
         }
-    }
-    rv
-}
-
-fn paths<'a>(
-    cave: &'a Cave,
-    cs: &'a CaveSystem,
-    visited: &mut HashSet<&'a Cave>,
-    path: &mut Vec<&'a Cave>,
-) -> usize {
-    if cave.0 == "end" {
-        // for c in path {
-        //     print!("{}-", c.0);
-        // }
-        // println!("end");
-        return 1;
-    }
-    path.push(cave);
-    if cave.is_small_cave() {
-        visited.insert(cave);
-    }
-    let mut rv = 0;
-    for con in cs.connections.get(cave).unwrap() {
-        if con.is_small_cave() && visited.contains(con) {
-            continue;
+        path.push(cave);
+        if cave.is_small_cave() {
+            let entry = visited.entry(cave).or_insert(0);
+            *entry += 1;
         }
-        rv += paths(con, cs, visited, path);
+        let mut rv = 0;
+        for con in cs.connections.get(cave).unwrap() {
+            if con.0 == "start" {
+                continue;
+            }
+            if visited.contains_key(con) {
+                if s_cave_used_twice {
+                    continue;
+                } else {
+                    // Let's use this small cave twice
+                    rv += paths2(con, cs, visited, path, true);
+                }
+            } else {
+                rv += paths2(con, cs, visited, path, s_cave_used_twice);
+            }
+        }
+        path.pop();
+        if cave.is_small_cave() {
+            let ref_count = visited.get_mut(cave).unwrap();
+            *ref_count -= 1;
+            if *ref_count == 0 {
+                visited.remove(cave);
+            }
+        }
+        rv
     }
-    path.pop();
-    visited.remove(cave);
-    rv
-}
 
-fn part1(cs: &CaveSystem) -> usize {
-    let start = cs.caves.get(&Cave(String::from("start"))).unwrap();
-    paths(start, cs, &mut HashSet::new(), &mut Vec::new())
-}
+    fn paths<'a>(
+        cave: &'a Cave,
+        cs: &'a CaveSystem,
+        visited: &mut HashSet<&'a Cave>,
+        path: &mut Vec<&'a Cave>,
+    ) -> usize {
+        if cave.0 == "end" {
+            // for c in path {
+            //     print!("{}-", c.0);
+            // }
+            // println!("end");
+            return 1;
+        }
+        path.push(cave);
+        if cave.is_small_cave() {
+            visited.insert(cave);
+        }
+        let mut rv = 0;
+        for con in cs.connections.get(cave).unwrap() {
+            if con.is_small_cave() && visited.contains(con) {
+                continue;
+            }
+            rv += paths(con, cs, visited, path);
+        }
+        path.pop();
+        visited.remove(cave);
+        rv
+    }
 
-fn part2(cs: &CaveSystem) -> usize {
-    let start = cs.caves.get(&Cave(String::from("start"))).unwrap();
-    paths2(start, cs, &mut HashMap::new(), &mut Vec::new(), false)
-}
+    pub fn part_1(cs: &CaveSystem) -> usize {
+        let start = cs.caves.get(&Cave(String::from("start"))).unwrap();
+        paths(start, cs, &mut HashSet::new(), &mut Vec::new())
+    }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    pub fn part_2(cs: &CaveSystem) -> usize {
+        let start = cs.caves.get(&Cave(String::from("start"))).unwrap();
+        paths2(start, cs, &mut HashMap::new(), &mut Vec::new(), false)
+    }
 
-    const TEST_INPUT: &str = "start-A
+    pub fn generator(input: &str) -> CaveSystem {
+        CaveSystem::new(input)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        const TEST_INPUT: &str = "start-A
 start-b
 A-c
 A-b
@@ -142,22 +142,19 @@ b-d
 A-end
 b-end";
 
-    #[test]
-    #[ignore]
-    fn test_parse() {
-        dbg!(CaveSystem::new(&TEST_INPUT));
-    }
+        #[test]
+        fn test_parse() {
+            dbg!(CaveSystem::new(&TEST_INPUT));
+        }
 
-    #[test]
-    #[ignore]
-    fn test_part1() {
-        assert_eq!(part1(&CaveSystem::new(&TEST_INPUT)), 10);
-    }
+        #[test]
+        fn test_part1() {
+            assert_eq!(part_1(&CaveSystem::new(&TEST_INPUT)), 10);
+        }
 
-    #[test]
-    #[ignore]
-    fn test_part1_1() {
-        let s = "dc-end
+        #[test]
+        fn test_part1_1() {
+            let s = "dc-end
 HN-start
 start-kj
 dc-start
@@ -167,20 +164,21 @@ HN-end
 kj-sa
 kj-HN
 kj-dc";
-        assert_eq!(part1(&CaveSystem::new(&s)), 19);
-    }
+            assert_eq!(part_1(&CaveSystem::new(&s)), 19);
+        }
 
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(&CaveSystem::new(&TEST_INPUT)), 36);
-    }
+        #[test]
+        fn test_part2() {
+            assert_eq!(part_2(&CaveSystem::new(&TEST_INPUT)), 36);
+        }
 
-    #[test]
-    fn test_part2_2() {
-        let s = "start-A
+        #[test]
+        fn test_part2_2() {
+            let s = "start-A
 A-b
 b-c
 c-end";
-        assert_eq!(part2(&CaveSystem::new(&s)), 2);
+            assert_eq!(part_2(&CaveSystem::new(&s)), 2);
+        }
     }
 }
